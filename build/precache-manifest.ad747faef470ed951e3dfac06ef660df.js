f2ab9e82ac03800630d630451c8f2680e9dc28db
nonnyboing <nonnyboing@gmail.com>
2020-05-04 00:09:47 +0100

    production build for the application

---
+++ build/precache-manifest.ad747faef470ed951e3dfac06ef660df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0785ef127622ebe2dfb0553584c87c74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efd2c955e016f6576c18",
+    "url": "/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "67880c4a8a409a6d80e2",
+    "url": "/static/css/main.1ede30a2.chunk.css"
+  },
+  {
+    "revision": "efd2c955e016f6576c18",
+    "url": "/static/js/2.f65c3a35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f65c3a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67880c4a8a409a6d80e2",
+    "url": "/static/js/main.a57cf429.chunk.js"
+  },
+  {
+    "revision": "477bc82afd223627d4d3",
+    "url": "/static/js/runtime-main.d3255406.js"
+  }
+]);
